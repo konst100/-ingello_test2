35dc2e9685f02085cab5e02a262477efe465e747
konst100 <schwezkonst@gmail.com>
2021-07-22 13:16:56 +0300

    send and form

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-<form action="send.php" method="post">
-<input type="text" name="fio" placeholder="Укажите ФИО" required>
-<input type="text" name="email" placeholder="Укажите e-mail" required>
-<input type="submit" value="Отправить">
-</form>
