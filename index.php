f44e18dc48f31b3e678203d2118adfccd8e71733
konst100 <schwezkonst@gmail.com>
2021-07-22 13:06:07 +0300

    form html

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
-$x = 100;
-
-echo $x;
+<form action="send.php" method="post">
+<input type="text" name="fio" placeholder="Укажите ФИО" required>
+<input type="text" name="email" placeholder="Укажите e-mail" required>
+<input type="submit" value="Отправить">
+</form>
